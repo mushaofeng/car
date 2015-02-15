b087e33f80fb17d7d0d8ff11401a152e33003b91
mushaofeng <moushaofeng@tudou.com>
2015-02-15 20:14:33 +0800

    add by mushaofeng

--- demo/js/car.js
+++ demo/js/car.js
@@ -1,6 +1,28 @@
 $(
 	function  () {
 
-$('.carousel').carousel()
+		$('.carousel').carousel()
+		$('#campareModal').modal({
+			backdrop:false,
+			keyboard:true,
+			show:false,
+			remote:false
+
+		})
+		$("#compareWrap .close").bind('click',function  (e) {
+			// console.log( e );
+			var $wrap=$(e.currentTarget).parent();
+			$wrap.fadeOut(function  () {
+				$wrap.remove();
+			});
+			var $num=$("#campareModal .cp-num");
+			var num=$num.text()*1-1;
+			$num.text(num);
+		}) 
+
+		$('#carTab a').click(function (e) {
+		  e.preventDefault();
+		  $(this).tab('show');
+		})		
 	}
 )
